fcfb2352f7c729468c08e7b47c846fa4223f562c
EduardSekan <sekaneduard@gmail.com>
2023-03-09 19:10:54 +0200

    chnages

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const app = require("./app")
 const mongoose = require("mongoose")
 
-const DB_HOST =
-    "mongodb+srv://Makato:rs3ISRes7zMzPaXD@cluster0.qkzzuvd.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+const app = require("./app")
+
+const { DB_HOST, PORT = 3000 } = require(process.env)
 
 mongoose.set("strictQuery", true)
 
 mongoose
     .connect(DB_HOST)
     .then(() => {
-        app.listen(3000)
+        app.listen(PORT)
     })
     .catch((error) => {
         console.log(error.message)
