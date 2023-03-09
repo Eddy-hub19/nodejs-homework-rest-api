1c09791bb34ae1328013ab8adf0f0b29fb5dd0a1
EduardSekan <sekaneduard@gmail.com>
2023-03-09 17:23:24 +0200

    fix problem with serverjs

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 // Makato
 // rs3ISRes7zMzPaXD
 
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const DB_HOST =
-    "mongodb+srv://Makato:rs3ISRes7zMzPaXD@cluster0.qkzzuvd.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+// const DB_HOST =
+//     "mongodb+srv://Makato:rs3ISRes7zMzPaXD@cluster0.qkzzuvd.mongodb.net/contacts_reader?retryWrites=true&w=majority"
 
-mongoose.set('strictQuery', true)
+// mongoose.set('strictQuery', true)
 
-mongoose
-    .connect(DB_HOST)
-    .then(() => console.log("Database connect success"))
-    .catch((error) => console.log(error.message))
+// mongoose
+//     .connect(DB_HOST)
+//     .then(() => console.log("Database connect success"))
+//     .catch((error) => console.log(error.message))
 
-console.log(process.env);
+// console.log(process);
