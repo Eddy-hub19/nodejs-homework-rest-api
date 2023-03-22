9efcf199e3b387512b301428a138cebe7afefc5d
EduardSekan <sekaneduard@gmail.com>
2023-03-22 21:14:23 +0200

    add hw-05-avatars

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,8 +1,15 @@
 const fs = require("fs/promises")
 const path = require("path")
 const { User } = require("../../models/user")
+const Jimp = require("jimp")
 
-const avatarsDir = path.join(__dirname, "../../", "public", "avatar")
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
+
+const sizeAvatar = async (path) => {
+    const avatar = await Jimp.read(path)
+    avatar.resize(250, 250)
+    await avatar.writeAsync(path)
+}
 
 const updateAvatar = async (req, res) => {
     const { _id } = req.user
@@ -12,7 +19,7 @@ const updateAvatar = async (req, res) => {
     await fs.rename(tempUpload, resultUpload)
     const avatarURL = path.join("public", "avatars", filename)
     await User.findByIdAndUpdate(_id, { avatarURL })
-
+    sizeAvatar(avatarURL)
     res.json({
         avatarURL,
     })
