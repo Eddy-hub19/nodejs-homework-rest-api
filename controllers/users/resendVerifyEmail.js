9eed2e4f5aadd78118695968c16495097219d507
EduardSekan <sekaneduard@gmail.com>
2023-03-24 22:28:53 +0200

    add hw

--- controllers/users/resendVerifyEmail.js
+++ controllers/users/resendVerifyEmail.js
@@ -7,10 +7,10 @@ const resendVerifyEmail = async (req, res) => {
     const { email } = req.body
     const user = await User.findOne({ email })
     if (!user) {
-        throw HttpError(400, "missing required field email")
+        throw HttpError(400, "Verification email sent")
     }
     if (user.verify) {
-        throw HttpError(401, "Email already verify")
+        throw HttpError(401, "Verification has already been passed")
     }
 
     const verifyEEmail = {
